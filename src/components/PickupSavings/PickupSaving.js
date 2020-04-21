db7dd1b72e3dda3e2b1e2f965270f95c305cb0b7
madu munachi <madustanley1@gmail.com>
2020-04-21 05:53:18 +0100

    modified

--- src/components/PickupSavings/PickupSaving.js
+++ src/components/PickupSavings/PickupSaving.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef, useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 const Row = styled.div `
@@ -13,18 +13,45 @@ const Col1 = styled.div `
 color: red;
 font-weight: 800;
 `
+const Des = styled.p `
+font-style: italic;
+color: red;
+`
 
-class PickupSavings extends React.Component {
-    render() {
+function useHover() {
+    const ref = useRef()
+    const [hovered, setHovered] = useState(false)
+
+    const enter = () => setHovered(true)
+    const leave = () => setHovered(false)
+
+    useEffect(() => {
+    ref.current.addEventListener('mouseenter', enter)
+    ref.current.addEventListener('mouseleave', leave)
+    return () => {
+        ref.current.removeEventListener('mouseenter', enter)
+        ref.current.removeEventListener('mouseenter', enter)
+    }
+    },
+    [ref]
+    )
+
+    return [ref, hovered]
+}
+
+function PickupSavings(props) {
+     
+        const [ref, hovered] = useHover()
         return (
-        <div>
+        <div ref={ref}>
             <Row>
               <Col md={6}>Pickup Savings </Col>
-       <Col1 md={6}>{`$${this.props.price}`} </Col1>
+       <Col1 md={6}>{`$${props.price}`} </Col1>
             </Row>
+            {hovered && <Des>Picking up your orders in the store helps cut costs, we pass the savings unto you.</Des>}
         </div>
         )
-    }
+    
 }
 
 export default PickupSavings
